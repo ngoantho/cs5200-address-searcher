a806977bf6cc4a0f6d159e2428003cef075d8d02
tk <dadakil83@gmail.com>
2024-03-04 12:08:37 -0800

    add customerModel.js to update zipcode in various type.

---
+++ models/customerModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const addressSchema = mongoose.Schema(
+    {
+        lastName: {
+            type: String, 
+            required: [true, "Please enter a last name"],
+        },
+
+        firstName: {
+            type: String, 
+            required: [true, "Please enter a first name"],
+        },
+
+        street: {
+            type: String,
+            required: [true, "Please enter a street"],
+            default: " ",
+        },
+        
+        city: {
+            type: String,
+            required: [true, "Please enter a City"],
+            default: " ",
+        },
+        
+        county: {
+            type: String,
+            required: [true, "Please enter a county"],
+            default: " ",
+        },
+        
+        state: {
+            type: String,
+            required: [true, "Please enter a state"],
+            default: " ",
+        },
+        
+        zip: {
+            type: string,
+            required: [true, "Please enter a zip code(postal code)"],
+        },
+        
+        country: {
+            type: String,
+            required: [true, "Please enter a country"],
+        },
+        
+        countryCode: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const address = mongoose.model('address', addressSchema);
+
+module.exports = address;
+
